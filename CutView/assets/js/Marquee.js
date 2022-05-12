03dad9fe58f91638dd7096ac11176300d1e85575
allen.liu <allen.liu@shinda.com.tw>
2022-05-12 18:26:25 +0800

    推

--- CutView/assets/js/Marquee.js
+++ CutView/assets/js/Marquee.js
@@ -2,20 +2,21 @@
 
 window.onload = function () {
     const MarqueeContent = [
-        '六月份排程尚餘，歡迎來電來信加Line預約',
-        '疫情影響百工待興，月初前六專案享',
-        '諮詢專線：02-XXXX-XXXXX'
+        '六月份排程尚餘，歡迎來電來信加Line預約。',
+        '疫情影響百工待興，月初前六專案享八五折優惠。',
+        '諮詢專線：02-XXXX-XXXXX。',
+        '近期工作量滿檔，暫時停止接案，謝謝。'
     ];
     let key = 0;
-    const marquee = $('.marquee');
-    marquee.on('animationstart', () => {
+    const Marquee = $('.marquee');
+    Marquee.on('animationstart', () => {
         key = 0;
-        marquee.text(MarqueeContent[key]);
+        Marquee.text(MarqueeContent[key]);
     });
-    marquee.on('animationiteration', () => {
+    Marquee.on('animationiteration', () => {
         key++;
         if (typeof MarqueeContent[key] === 'undefined') key = 0;
-        marquee.text(MarqueeContent[key]);
+        Marquee.text(MarqueeContent[key]);
     });
-    marquee.removeClass('paused');
+    Marquee.removeClass('paused');
 }
