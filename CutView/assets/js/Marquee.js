647f1b527bc70c572973feb0fbce7579120c96fa
allen.liu <allen.liu@shinda.com.tw>
2022-05-12 14:48:49 +0800

    合併

--- CutView/assets/js/Marquee.js
+++ CutView/assets/js/Marquee.js
@@ -1,21 +1,21 @@
 // 參考 https://stackoverflow.com/questions/49382321/how-to-display-multiple-contents-as-marquee-one-after-another-second-content-sh
 
-//list of slides to be shown
-const MarqueeContent = [
-    'first slide',
-    'second slide',
-    'third slide'
-];
-
-let key = 0;
-const marquee = $('.marquee');
-marquee.on('animationstart', () => {
-    key = 0;
-    marquee.text(content[key]);
-});
-marquee.on('animationiteration', () => {
-    key++;
-    if(typeof content[key] === 'undefined') key = 0;
-    marquee.text(content[key]);
-});
-marquee.removeClass('paused');
+window.onload = function () {
+    const MarqueeContent = [
+        '六月份排程尚餘，歡迎來電來信加Line預約',
+        '疫情影響百工待興，月初前六專案享',
+        '諮詢專線：02-XXXX-XXXXX'
+    ];
+    let key = 0;
+    const marquee = $('.marquee');
+    marquee.on('animationstart', () => {
+        key = 0;
+        marquee.text(MarqueeContent[key]);
+    });
+    marquee.on('animationiteration', () => {
+        key++;
+        if (typeof MarqueeContent[key] === 'undefined') key = 0;
+        marquee.text(MarqueeContent[key]);
+    });
+    marquee.removeClass('paused');
+}
