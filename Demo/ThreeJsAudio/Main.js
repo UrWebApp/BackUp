a9af9dc8ec064a830e7fec7224768b3188f72630
johch3n611u <401450650@gapp.fju.edu.tw>
2022-05-18 00:50:41 +0800

    audio

--- Demo/ThreeJsAudio/Main.js
+++
@@ -1,3 +0,0 @@
-import * as _THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js';
-import * as _OrbitControls from 'https://threejsfundamentals.org/threejs/resources/threejs/r114/examples/jsm/controls/OrbitControls.js';
-export { THREE, OrbitControls };
